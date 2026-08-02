d3f3abe3f1db58273b80af3a29ee05928f556267
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-08-02 13:52:46 +0800

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -45,7 +45,7 @@ export interface LanguageModel {
   id: string;
   name: string;
   description: string;
-  backend: 'llama-cpp' | 'ollama' | 'openai' | 'other';
+  backend: 'llama-cpp' | 'ollama' | `deepseek` | 'openai' | 'other';
   contextLength: number;
   model: string; // Path to the language model.
   mmproj: string; // Path to the language model's mmproj.
