a5294456ed63c95c3cc6a9fbdf3d626415a1dc1f
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-08-16 00:28:22 +0800

    Updates

--- src/configurations.ts
+++ src/configurations.ts
@@ -1,5 +1,5 @@
-export const address = `http://${import.meta.env.VITE_HOST_IP}`
+export const localAddress = `http://${import.meta.env.VITE_HOST_IP}`
 
-export const port = "3001"
+export const localPort = "3001"
 
-export const localURL = `${address}:${port}`
+export const localURL = `${localAddress}:${localPort}`
