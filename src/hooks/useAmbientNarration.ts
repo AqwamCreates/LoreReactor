df57517f89c0aedac3eadbd44d409399381be02d
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-09-23 15:19:01 +0800

    Update useAmbientNarration.ts

--- src/hooks/useAmbientNarration.ts
+++ src/hooks/useAmbientNarration.ts
@@ -94,7 +94,7 @@ export function useAmbientNarration(
             await new Promise(r => setTimeout(r, 20));
         }
 
-        return addMessageToInteractionData(data, createChatMessage(data, AMBIENT_NARRATOR, selected));
+        return addMessageToInteractionData(data, createChatMessage(data, AMBIENT_NARRATOR, selected, { isPartial: false }));
     }, [setStreamingState, setStreamingText, streamingTextRef]);
 
     return { generateAmbientNarration, AMBIENT_NARRATOR };
